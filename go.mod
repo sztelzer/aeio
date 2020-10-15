5f542c004ae9d9bcbbb8816a414431f6e3db7622
sztelzer <sztelzer@gmail.com>
2020-10-14 23:16:39 -0300

    firebase auth ok

--- go.mod
+++ go.mod
@@ -3,13 +3,7 @@ module github.com/sztelzer/aeio
 go 1.14
 
 require (
-	cloud.google.com/go v0.68.0 // indirect
 	cloud.google.com/go/datastore v1.3.0
-	go.opencensus.io v0.22.5 // indirect
-	golang.org/x/net v0.0.0-20201006153459-a7d1128ccaa0 // indirect
-	golang.org/x/sys v0.0.0-20201008064518-c1f3e3309c71 // indirect
-	golang.org/x/tools v0.0.0-20201008184944-d01b322e6f06 // indirect
-	google.golang.org/api v0.32.0
-	google.golang.org/genproto v0.0.0-20201008135153-289734e2e40c // indirect
-	google.golang.org/grpc v1.33.0 // indirect
+	firebase.google.com/go/v4 v4.0.0
+	google.golang.org/api v0.33.0
 )
--- init.go
+++ init.go
@@ -3,6 +3,8 @@ package aeio
 import (
 	"cloud.google.com/go/datastore"
 	"context"
+	firebase "firebase.google.com/go/v4"
+	firebaseAuth "firebase.google.com/go/v4/auth"
 	"log"
 )
 
@@ -12,13 +14,29 @@ var Context context.Context
 var ContextCancel context.CancelFunc
 var DatastoreClient *datastore.Client
 
+var FireApp *firebase.App
+var FireAppAuthClient *firebaseAuth.Client
+
+
 func init() {
 	var err error
 	Context, ContextCancel = context.WithCancel(context.Background())
 	DatastoreClient, err = datastore.NewClient(Context, datastore.DetectProjectID)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error initializing datastore client: %v", err)
 	}
+
+	// in localhost it gets the key.json from ENV
+	FireApp, err = firebase.NewApp(Context, nil)
+	if err != nil {
+		log.Fatalf("error initializing firebase app: %v", err)
+	}
+
+	FireAppAuthClient, err = FireApp.Auth(Context)
+	if err != nil {
+		log.Fatalf("error initializing firebase auth client: %v", err)
+	}
+
 }
 
 func ShutdownApplication() {
